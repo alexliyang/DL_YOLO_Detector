8c7e6aa56e9bbeca7f345fc19040d2884d593e94
kpiaskowski <kar.piaskowski@gmail.com>
2018-02-14 15:47:21 +0100

    tf records added

--- cell_net.py
+++ cell_net.py
@@ -6,8 +6,9 @@ import numpy as np
 import tensorflow as tf
 
 from architecture.convolution import conv_model
-from cell_net_utils import resize_label, image_read, embed_output, possibly_create_dirs
+from cell_net_utils import resize_label, image_read, embed_output, possibly_create_dirs, decode_train_data, augment_rotate, augment_crop, augment_translate, augment_stack, create_augmented_tf_records
 from parameters import params
+
 
 # use if data was not generated
 # generate_cell_net_data('cell_data', params.img_size, params.name_converter, params.classes)
@@ -18,6 +19,11 @@ eta = 0.00001
 threshold_area = int(params.img_size / S) ** 2 / 2
 batch_size = 5
 epochs = 50
+tfrecord_length = 10
+capacity = 100
+num_threads = 1
+min_after_deque = 20
+augmentations = 1# number of dataset augmentations
 
 # paths
 yolo_weights_path = 'models/yolo_pretrained/YOLO_small.ckpt'
@@ -28,21 +34,26 @@ t_images_path = 'cell_data/train_images/'
 t_labels_path = 'cell_data/train_labels/'
 v_images_path = 'cell_data/val_images/'
 v_labels_path = 'cell_data/val_labels/'
+train_records_path = 'cell_data/train_records/'
 embedded_images_path = 'cell_data/output_images/'
 possibly_create_dirs(embedded_images_path, model_to_save_path)
 
-# data
+# delete if generated!!
 train_image_filenames = sorted([t_images_path + name for name in os.listdir(t_images_path)])
 train_labels_filenames = sorted([t_labels_path + name for name in os.listdir(t_labels_path)])
+for i in range(augmentations):
+    create_augmented_tf_records(i, train_image_filenames, train_labels_filenames, train_records_path, tfrecord_length , S, threshold_area)
+
 val_image_filenames = sorted([v_images_path + name for name in os.listdir(v_images_path)])
 val_labels_filenames = sorted([v_labels_path + name for name in os.listdir(v_labels_path)])
-
-train_data_len = len(train_image_filenames)
 val_data_len = len(val_image_filenames)
-num_batches_t = train_data_len // batch_size
+
+num_batches_t = len(os.listdir(train_records_path)) * augmentations * tfrecord_length // batch_size
 num_batches_v = val_data_len // batch_size
 
 # model
+train_images, train_labels = decode_train_data(train_records_path, S, batch_size, capacity,
+                                                               num_threads, min_after_deque)
 images_placeholder = tf.placeholder(dtype=tf.float32, shape=[None, params.img_size, params.img_size, 3])
 labels_placeholder = tf.placeholder(dtype=tf.float32, shape=[None, S, S, params.C])
 conv = conv_model(images_placeholder)
@@ -64,7 +75,8 @@ train_op = tf.train.AdamOptimizer(eta).minimize(loss)
 merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+    sess.run(init_op)
     saver = tf.train.Saver()
 
     # loading models
@@ -75,54 +87,47 @@ with tf.Session() as sess:
         saver_yolo = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='yolo'))
         saver_yolo.restore(sess, yolo_weights_path)
         print('YOLO model loaded')
-
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
     train_writer = tf.summary.FileWriter('summaries/cell_network/' + model_to_save_path.replace('models/', '') + '_T',
                                          sess.graph)
     val_writer = tf.summary.FileWriter('summaries/cell_network/' + model_to_save_path.replace('models/', '') + '_V',
                                        sess.graph)
 
-    for epoch in range(100):
-        idx = randint(0, val_data_len - 1)
-        image = image_read(train_image_filenames[idx])
-        label = resize_label(np.load(train_labels_filenames[idx]), S, params.C, params.img_size, threshold_area)
-        embedded_output = embed_output((image + 1) / 2, label, 0.3, S, params.img_size)
-        cv2.imshow('s', embedded_output)
-        cv2.waitKey(2000)
-        # cv2.imwrite(embedded_images_path + '_' + str(epoch) + '_' + str(epoch) + '.jpg',
-        #             embedded_output * 255.0)
+    for epoch in range(epochs):
+        for batch_idx in range(num_batches_t):
+            # read from tf records
+            images, labels = sess.run([train_images, train_labels])
+            _, cost, summary = sess.run([train_op, loss, merged], feed_dict={images_placeholder: images,
+                                                                             labels_placeholder: labels})
+            print('train', epoch, batch_idx, cost)
+            train_writer.add_summary(summary, epoch * batch_size + batch_idx)
+            train_writer.flush()
 
-    # for epoch in range(epochs):
-    #     for batch_idx in range(10):
-    #         ids = np.random.choice(range(train_data_len), batch_size)
-    #         images = [image_read(train_image_filenames[i]) for i in ids]
-    #         labels = [resize_label(np.load(train_labels_filenames[i]), S, params.C, params.img_size, threshold_area) for
-    #                   i in ids]
-    #
-    #         _, cost, summary = sess.run([train_op, loss, merged], feed_dict={images_placeholder: images,
-    #                                                                          labels_placeholder: labels})
-    #         print('train', epoch, batch_idx, cost)
-    #         train_writer.add_summary(summary, epoch * batch_size + batch_idx)
-    #         train_writer.flush()
-    #
-    #         if batch_idx % 100 == 0:
-    #             image = image_read(val_image_filenames[randint(0, val_data_len - 1)])
-    #             output = sess.run(sigmoid_output, feed_dict={images_placeholder: [image]})
-    #             embedded_output = embed_output((image + 1) / 2, output[0], 0.3, S, params.img_size)
-    #             cv2.imwrite(embedded_images_path + '_' + str(epoch) + '_' + str(batch_idx) + '.jpg',
-    #                         embedded_output * 255.0)
-    #
-    #     for batch_idx in range(10):
-    #         ids = np.random.choice(range(val_data_len), batch_size)
-    #         images = [image_read(val_image_filenames[i]) for i in ids]
-    #         labels = [resize_label(np.load(val_labels_filenames[i]), S, params.C, params.img_size, threshold_area) for i
-    #                   in ids]
-    #
-    #         summary = sess.run(merged, feed_dict={images_placeholder: images,
-    #                                               labels_placeholder: labels})
-    #         print('validation', epoch, batch_idx)
-    #         val_writer.add_summary(summary, epoch * batch_size + batch_idx)
-    #         val_writer.flush()
-    #
-    #     saver.save(sess, os.path.join(model_to_save_path, 'model.ckpt'))
-    #     if epoch % 10 == 0 and epoch > 0:
-    #         saver.save(sess, os.path.join(model_to_save_path, str(epoch) + '_model.ckpt'))
+            if batch_idx % 100 == 0:
+                image = image_read(val_image_filenames[randint(0, val_data_len - 1)])
+                output = sess.run(sigmoid_output, feed_dict={images_placeholder: [image]})
+                embedded_output = embed_output((image + 1) / 2, output[0], 0.3, S, params.img_size)
+                cv2.imwrite(embedded_images_path + '_' + str(epoch) + '_' + str(batch_idx) + '.jpg',
+                            embedded_output * 255.0)
+
+        # read from disc
+        for batch_idx in range(num_batches_v):
+            ids = np.random.choice(range(val_data_len), batch_size)
+            images = [image_read(val_image_filenames[i]) for i in ids]
+            labels = [resize_label(np.load(val_labels_filenames[i]), S, params.C, params.img_size, threshold_area) for i
+                      in ids]
+
+            summary = sess.run(merged, feed_dict={images_placeholder: images,
+                                                  labels_placeholder: labels})
+            print('validation', epoch, batch_idx)
+            val_writer.add_summary(summary, epoch * batch_size + batch_idx)
+            val_writer.flush()
+
+        saver.save(sess, os.path.join(model_to_save_path, 'model.ckpt'))
+        if epoch % 10 == 0 and epoch > 0:
+            saver.save(sess, os.path.join(model_to_save_path, str(epoch) + '_model.ckpt'))
+
+    coord.request_stop()
+    coord.join(threads)
+    sess.close()
--- cell_net_utils.py
+++ cell_net_utils.py
@@ -1,7 +1,11 @@
+import random
+import tensorflow as tf
 import numpy as np
 import xml.etree.ElementTree as ET
 import cv2
 import pickle
+from parameters import params
+
 from sklearn.utils import shuffle
 import os
 def xml_as_tensor(xml_path, dst_img_size, name_converter, classes):
@@ -82,8 +86,6 @@ def generate_cell_net_data(root_folder, img_size, name_converter, classes):
         cv2.imwrite(os.path.join(v_images_dir, str(i) + '.jpg'), img)
         np.save(os.path.join(v_labels_dir, str(i) + '.npy'), label)
 
-
-
 def resize_label(label, S, C, src_img_size, threshold_area):
     resized_label = np.zeros([S, S, C], dtype=np.float32)
     for y in range(S):
@@ -137,3 +139,160 @@ def possibly_create_dirs(embedded_images_path, model_to_save_path):
         os.mkdir(embedded_images_path)
     if not os.path.isdir(model_to_save_path):
         os.mkdir(model_to_save_path)
+
+def augment_rotate(image, label):
+    """
+    Rotates both image and label k times 90 degress
+    :param label: original label (not resized
+    :return:
+    """
+    k = random.randint(0, 4)
+    return np.rot90(image, k), np.rot90(label, k)
+
+def augment_crop(image, label, k):
+    """
+    Crops image and resizes it, remains label intact
+    :param label: original label (not resized
+    :param k: maximum cropping range from each side
+    :return:
+    """
+    h, w = image.shape[:2]
+    return cv2.resize(image[random.randint(0, k): h-random.randint(0, k), random.randint(0, k): w-random.randint(0, k)], (h, w)), label
+
+def augment_translate(image, label, k):
+    """
+    Rolls both image and label k pixels maximum in each direction (but only if it would not cause to roll bounding box)
+    :param label: original label (not resized
+    :param k: maximum roll range
+    :return:
+    """
+    flat_label = np.max(label, axis=2)
+    non_zero_ys, non_zero_xs = np.nonzero(flat_label)
+    y_low_margin = np.min(non_zero_ys)
+    y_high_margin =  image.shape[0] - np.max(non_zero_ys)
+    x_low_margin = np.min(non_zero_xs)
+    x_high_margin = image.shape[1] - np.max(non_zero_xs)
+    d_y = np.min([k, y_low_margin, y_high_margin])
+    d_x = np.min([k, x_low_margin, x_high_margin])
+    vertical_shift = random.randint(-d_y, d_y)
+    horizontal_shift = random.randint(-d_x, d_x)
+
+    image = np.roll(image, axis=0, shift=vertical_shift)
+    image = np.roll(image, axis=1, shift=horizontal_shift)
+    label = np.roll(label, axis=0, shift=vertical_shift)
+    label = np.roll(label, axis=1, shift=horizontal_shift)
+    return image, label
+
+def augment_stack(images, labels):
+    """
+    Stacks four images into one, then resizes both image and label
+    :return:
+    """
+    images, labels = shuffle(images, labels)
+
+    images_upper_stack = np.hstack([images[0], images[1]])
+    images_lower_stack = np.hstack([images[2], images[3]])
+    image_stack = np.vstack([images_upper_stack, images_lower_stack])
+
+    labels_upper_stack = np.hstack([labels[0], labels[1]])
+    labels_lower_stack = np.hstack([labels[2], labels[3]])
+    label_stack = np.vstack([labels_upper_stack, labels_lower_stack])
+
+    # resize label to single image size
+    label_stack = resize_label(label_stack, images[0].shape[0], labels[0].shape[-1], image_stack.shape[0], 0.5)
+    image_stack = cv2.resize(image_stack, (images[0].shape[0], images[0].shape[0]))
+    return image_stack, label_stack
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def create_record_writer(writer_path, record_name):
+    """
+    Creates tfrecord writer in writer_path and assigns a name to it
+    """
+    writer = tf.python_io.TFRecordWriter(os.path.join(writer_path, record_name + '.tfrecord'))
+    return writer
+
+def create_augmented_tf_records(augmentation_number, image_names, label_names, data_folder, record_size, S, threshold_area):
+    """
+    Creates tf records of size record_size (with augmentation) and puts them into data folder
+    """
+    if not os.path.isdir(data_folder):
+        os.mkdir(data_folder)
+
+    beginning_index = len(os.listdir(data_folder))
+    image_names, label_names = shuffle(image_names, label_names)
+
+    # for stacking
+    images_buffer = []
+    labels_buffer = []
+
+    tf_records_count = 0
+    writer = create_record_writer(data_folder, str(beginning_index))
+
+    for i, (image_name, label_name) in enumerate(zip(image_names, label_names)):
+        print("\rAugmentation %d, generating train TFRecords (%.2f)" % (augmentation_number, i / len(image_names)), end='', flush=True)
+        image = image_read(image_name)
+        label = np.load(label_name)
+
+        # augmentation pipeline
+        image, label = augment_rotate(image, label)
+        image, label = augment_crop(image, label, 10)
+        image, label = augment_translate(image, label, 100)
+        image = image.astype(np.float32)
+
+        # probability of adding to buffer
+        if np.random.uniform(0, 1) < 0.2:
+            images_buffer.append(image)
+            labels_buffer.append(label)
+
+        label = resize_label(label, S, params.C, params.img_size, threshold_area)
+        feature = {'train/label': _bytes_feature(tf.compat.as_bytes(label.tostring())),
+                   'train/image': _bytes_feature(tf.compat.as_bytes(image.tostring()))}
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+        writer.write(example.SerializeToString())
+
+        if len(images_buffer) == 4:
+            image, label = augment_stack(images_buffer, labels_buffer)
+            label = resize_label(label, S, params.C, params.img_size, threshold_area)
+            feature = {'train/label': _bytes_feature(tf.compat.as_bytes(label.tostring())),
+                       'train/image': _bytes_feature(tf.compat.as_bytes(image.tostring()))}
+            example = tf.train.Example(features=tf.train.Features(feature=feature))
+            writer.write(example.SerializeToString())
+            images_buffer = []
+            labels_buffer = []
+
+        if i % record_size == 0 and i > 0:
+            writer.close()
+            tf_records_count += 1
+            writer = create_record_writer(data_folder, str(beginning_index + tf_records_count))
+    writer.close()
+    print()
+
+def decode_train_data(data_path, S, batch_size, capacity, num_threads, min_after_deque):
+    """
+    Decodes detection tf records on the fly.
+    capacity, num_threads, min_after_deque - for shuffle batch
+    :param batch_size:
+    :param mode: 'train' or 'validation'
+    :return:
+    """
+    feature = {'train/image': tf.FixedLenFeature([], tf.string),
+               'train/label': tf.FixedLenFeature([], tf.string)}
+
+    filenames = [os.path.join(data_path, name) for name in os.listdir(data_path)]
+    print(filenames)
+    filename_queue = tf.train.string_input_producer(filenames)
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example, features=feature)
+    image = tf.reshape(tf.decode_raw(features['train/image'], tf.float32), [params.img_size, params.img_size, 3])
+    label = tf.reshape(tf.decode_raw(features['train/label'], tf.float32), [S, S, params.C])
+
+    images, labels = tf.train.shuffle_batch([image, label],
+                                            batch_size=batch_size,
+                                            capacity=capacity,
+                                            num_threads=num_threads,
+                                            min_after_dequeue=min_after_deque,
+                                            allow_smaller_final_batch=True)
+    return images, labels
