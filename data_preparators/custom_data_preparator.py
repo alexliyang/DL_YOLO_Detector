2785fa720372196a24e8b5e7d9f17fd7744c8653
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 05:47:03 +0000

    Adding tarfile member sanitization to extractall()

--- data_preparators/custom_data_preparator.py
+++ data_preparators/custom_data_preparator.py
@@ -23,7 +23,26 @@ class CustomDataPreparator(DataPreparator):
             download_file_from_google_drive('1UACRpbi9vvbMiF7p6du8c-532woNExGf', path)
             print('ImageNet data downloaded, extracting..')
             with tarfile.open(path) as tar:
-                tar.extractall(self.data_root_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, self.data_root_path)
             print('Cleaning unnecesary files')
 
             # rename folders instead of copying (allows to preserve inheritance of python)
--- data_preparators/imagenet_preparator.py
+++ data_preparators/imagenet_preparator.py
@@ -21,7 +21,26 @@ class ImagenetPreparator(DataPreparator):
             download_file_from_google_drive('1rkqYfK378ixwvEmHUjB_tXU8kua3DwKm', tars_path)
             print('ImageNet data downloaded, extracting..')
             with tarfile.open(tars_path) as tar:
-                tar.extractall(self.data_root_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, self.data_root_path)
             print('Cleaning unnecesary files')
             os.remove(tars_path)
 
